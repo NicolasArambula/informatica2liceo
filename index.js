0f0e53380acc6f595433ce2cc2273160a78ce4e9
NicolasArambula <92396235+NicolasArambula@users.noreply.github.com>
2021-11-08 14:43:46 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+imput1=prompt("Ingrese un numero")
+imput2=prompt("Ingrese otro numero")
+numero1= Number(imput1)
+numero2= Number(imput2)
+suma = numero1 + numero2;
+resta = numero1 - numero2
+multiplicacion = numero1*numero2
+division = numero1/numero2
+console.log("La suma de los dos numeros es " + suma)
+console.log("La resta " + numero1 + "-" + numero2 + " es " + resta)
+console.log("La multiplicación es " + multiplicacion)
+console.log("La división " + numero1 + "/" + numero2 + " es " + division)

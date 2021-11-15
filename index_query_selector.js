dedeeb965fd69621f978280ee4383dc51ce60bdc
NicolasArambula <92396235+NicolasArambula@users.noreply.github.com>
2021-11-15 09:20:14 -0500

    Delete index_query_selector.js

--- index_query_selector.js
+++
@@ -1,6 +0,0 @@
-nombre = String(prompt("Cual es su nombre?"))
-apellido = String(prompt("Cual es su Apellido?"))
-edad1 = Number(prompt("Cual es su edad?"))
-edad2= edad1+10;
-document.getElementById("saludo").innerHTML = "Hola "+nombre+" "+ apellido;
-document.getElementById("años").innerHTML = "En 10 años usted tendrá "+edad2+" años";
